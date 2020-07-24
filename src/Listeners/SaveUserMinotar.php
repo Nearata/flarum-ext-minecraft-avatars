2293b0aab94c053fc990e6a95bce0eea2f6a8f51
William Di Cicco <nearata@protonmail.com>
2020-07-24 02:03:39 +0200

    Should fix undefined index: minotar
    * Better check

--- src/Listeners/SaveUserMinotar.php
+++ src/Listeners/SaveUserMinotar.php
@@ -4,15 +4,18 @@ namespace Nearata\MinecraftAvatars\Listeners;
 
 use Flarum\Foundation\ValidationException;
 use Flarum\User\Event\Saving;
+use Illuminate\Support\Arr;
 
 class SaveUserMinotar
 {
     public function handle(Saving $event)
     {
         $user = $event->user;
-        $minotar = $event->data['attributes']['minotar'];
+        $attributes = Arr::get($event->data, 'attributes', []);
 
-        if (isset($minotar)) {
+        if (array_key_exists('minotar', $attributes)) {
+            $minotar = $attributes['minotar'];
+
             $validUsernameRegex = "[a-zA-Z0-9_]{1,16}";
             $validUUIDPlainRegex = "[0-9a-f]{32}";
             $validUUIDDashRegex = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
